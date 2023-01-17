e39dc484249e5e76a88315a4d5fa49c8940273cb
Ana Garcia <anagarcia.vancouver@gmail.com>
2023-01-17 14:55:27 -0800

    Adjust BG and Fonts in Mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,22 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
 import {
+  useFonts, 
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
   Inter_800ExtraBold
 } from '@expo-google-fonts/inter'
-import { useFonts } from '@expo-google-fonts/inter/useFonts';
-import { Loading } from './src/assets/components/Loading';
+
+import { Loading } from './src/components/Loading';
 
 export default function App() {
 
   const [fontsLoaded] = useFonts ({
     Inter_400Regular,
-  Inter_600SemiBold,
-  Inter_700Bold,
-  Inter_800ExtraBold
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
   });
 
   if (!fontsLoaded) {
@@ -27,8 +27,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text} > Open up App.tsx to start working on your app!</Text>
+      <StatusBar barStyle="light-content" backgroundColor= "transparent" translucent />
     </View>
   );
 }
@@ -36,8 +36,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    color: 'white',
+    fontFamily: 'Inter_600SemiBold',
+  }
+
 });
