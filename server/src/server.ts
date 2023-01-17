2886e75e39bd02e95e0f5eb739eb51e2fde9494c
Ana Garcia <anagarcia.vancouver@gmail.com>
2023-01-16 21:59:03 -0800

    Finalized the Server Set Up

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,28 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors'
 
 const app = Fastify();
+const prisma = new PrismaClient()
 
 /** 
  * Metodo HTTP: Get, Put, Patch, Delete
 */
 
-app.get('/', () => {
-    return 'Hello World'
+app.register(cors)
+
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany({
+       
+    })
+
+    return habits
 })
 
 app.listen({
     port: 3333,
+}).then(( ) => {
+    
+    console.log ('HTTPS Server Running')
+
 })
---
+++ web/src/components/Habit.tsx
@@ -0,0 +1,5 @@
+export function Habit() {
+    return (
+        <Habit/>
+    )
+}
