1402e5b558a8fc86f6f0c914e976b9c4f4920b62
Ana Garcia <anagarcia.vancouver@gmail.com>
2023-01-16 22:51:37 -0800

    Add Styles

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -1,5 +1,11 @@
-export function Habit() {
+interface HabitProps {
+    completed: number
+}
+
+export function Habit(props: HabitProps) {
     return (
-        <Habit/>
+        <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center ">
+            {props.completed}
+        </div> 
     )
 }
